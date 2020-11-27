bf52902d889d8bd3be021ce4d6f0f7ab7b4689eb
magus123 <67945251+magus123@users.noreply.github.com>
2020-11-27 16:46:56 +0700

    magus123.github.io

---
+++ serviceworker.js
@@ -0,0 +1,48 @@
+var CACHE_NAME = 'my-site-cache-v2';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/js/jquery.min.js',
+  '/js/main.js',
+  '/images/pwa.png'
+
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('In Install ServiceWorker... Opened cache!');
+        return cache.addAll(urlsToCache);
+      })
+  );
+}); 
+
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+                cacheNames.filter(function(cacheName){
+                    return cacheName != CACHE_NAME
+                }).map(function (cacheName){
+                    return caches.delete(cacheName)
+                })  
+             );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
